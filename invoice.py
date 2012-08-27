0267c1b6222a12886ab0086146ceaaa5c885c1f4
resteve <resteve@zikzakmedia.com>
2012-08-27 11:31:51 +0200

    Readme and Legal notice

--- invoice.py
+++ invoice.py
@@ -1,5 +1,6 @@
-#This file is part of Tryton.  The COPYRIGHT file at the top level of
-#this repository contains the full copyright notices and license terms.
+#This file is part account_invoice_prevent_duplicates module for Tryton.
+#The COPYRIGHT file at the top level of this repository contains 
+#the full copyright notices and license terms.
 from trytond.model import Workflow, ModelView, ModelSQL
 from trytond.transaction import Transaction
 from trytond.pool import Pool
