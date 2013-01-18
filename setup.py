8f40094281342509419005c6e474572877b397b0
?ngel ?lvarez <angel@nan-tic.com>
2013-01-18 16:43:29 +0100

    [FIX] encoding

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# -*- encoding: utf-8 -*-
 #This file is part account_invoice_prevent_duplicates module for Tryton.
 #The COPYRIGHT file at the top level of this repository contains 
 #the full copyright notices and license terms.
