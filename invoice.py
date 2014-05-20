a64e5c27d6a1324b9908a29e329b1a6b72e68fcc
jmartin <jmartin@zikzakmedia.com>
2014-05-20 17:22:11 +0200

    Adapt signature of "def write()"

--- invoice.py
+++ invoice.py
@@ -22,38 +22,42 @@ class Invoice:
                 })
 
     @classmethod
-    def write(cls, invoices, vals):
-        super(Invoice, cls).write(invoices, vals)
-        if vals.get('state') == 'posted':
-            pool = Pool()
-            Translation = pool.get('ir.translation')
-            for invoice in invoices:
-                if not invoice.type in ('in_invoice','in_credit_note'):
-                    continue
-                domain = []
-                domain.append(('party', '=', invoice.party.id))
-                domain.append(('type', '=', invoice.type))
-                domain.append(('invoice_date', '=', invoice.invoice_date))
-                domain.append(('reference', '=', invoice.reference))
-                domain.append(('state', 'in', ('posted','paid')))
-                domain.append(('company', '=', invoice.company.id))
-                invoices = cls.search(domain)
-                if len(invoices) > 1:
-                    language = Transaction().language
-                    error = cls._error_messages['party_invoice_reference']
-                    message = Translation.get_source('account.invoice',
-                        'error', language, error)
-                    if not message:
-                        message = Translation.get_source(error, 'error',
-                            language)
-                    if message:
-                        error = message
-                    text = []
-                    for invoice in cls.browse(invoices):
-                        text.append(error % {
-                                'invoice': invoice.rec_name or '',
-                                'party': invoice.party.name,
-                                'reference': invoice.reference,
-                                })
-                    text = '\n\n'.join( text )
-                    cls.raise_user_error('duplicate_invoice', (text,))
+    def write(cls, *args):
+        pool = Pool()
+        Translation = pool.get('ir.translation')
+        language = Transaction().language
+
+        super(Invoice, cls).write(*args)
+
+        actions = iter(args)
+        for invoices, values in zip(actions, actions):
+            if values.get('state') == 'posted':
+                for invoice in invoices:
+                    if not invoice.type in ('in_invoice', 'in_credit_note'):
+                        continue
+                    domain = []
+                    domain.append(('party', '=', invoice.party.id))
+                    domain.append(('type', '=', invoice.type))
+                    domain.append(('invoice_date', '=', invoice.invoice_date))
+                    domain.append(('reference', '=', invoice.reference))
+                    domain.append(('state', 'in', ('posted', 'paid')))
+                    domain.append(('company', '=', invoice.company.id))
+                    invoices = cls.search(domain)
+                    if len(invoices) > 1:
+                        error = cls._error_messages['party_invoice_reference']
+                        message = Translation.get_source('account.invoice',
+                            'error', language, error)
+                        if not message:
+                            message = Translation.get_source(error, 'error',
+                                language)
+                        if message:
+                            error = message
+                        text = []
+                        for invoice in cls.browse(invoices):
+                            text.append(error % {
+                                    'invoice': invoice.rec_name or '',
+                                    'party': invoice.party.name,
+                                    'reference': invoice.reference,
+                                    })
+                        text = '\n\n'.join(text)
+                        cls.raise_user_error('duplicate_invoice', (text,))
