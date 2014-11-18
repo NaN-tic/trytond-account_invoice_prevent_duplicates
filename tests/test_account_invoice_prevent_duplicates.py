cf78b5fb3961d6214283245d9068255aaa840346
resteve <resteve@zikzakmedia.com>
2014-11-18 08:55:57 +0100

    FIX header module

--- tests/test_account_invoice_prevent_duplicates.py
+++ tests/test_account_invoice_prevent_duplicates.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# This file is part helloword module for Tryton.
+# This file is part account_invoice_prevent_duplicates module for Tryton.
 # The COPYRIGHT file at the top level of this repository contains
 # the full copyright notices and license terms.
 import unittest
