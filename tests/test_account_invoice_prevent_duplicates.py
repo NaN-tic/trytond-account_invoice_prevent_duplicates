1f038face577805819b40c293a924fe9a681f07f
resteve <resteve@zikzakmedia.com>
2015-12-01 10:57:59 +0100

    Tests

--- tests/test_account_invoice_prevent_duplicates.py
+++ tests/test_account_invoice_prevent_duplicates.py
@@ -1,22 +1,14 @@
-#!/usr/bin/env python
-# This file is part account_invoice_prevent_duplicates module for Tryton.
-# The COPYRIGHT file at the top level of this repository contains
-# the full copyright notices and license terms.
+# This file is part of the account_invoice_prevent_duplicates module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains the full
+# copyright notices and license terms.
 import unittest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_depends
+from trytond.tests.test_tryton import ModuleTestCase
 
 
-class AccountInvoicePreventDuplicatesTestCase(unittest.TestCase):
+class AccountInvoicePreventDuplicatesTestCase(ModuleTestCase):
     'Test Account Invoice Prevent Duplicates module'
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module(
-            'account_invoice_prevent_duplicates')
-
-    def test0006depends(self):
-        'Test depends'
-        test_depends()
+    module = 'account_invoice_prevent_duplicates'
 
 
 def suite():
