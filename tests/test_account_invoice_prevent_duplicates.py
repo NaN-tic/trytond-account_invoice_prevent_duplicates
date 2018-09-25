0d2de344242ef29f4ddf20aff84f4ef499d6d273
Raimon Esteve <raimon@nan-tic.com>
2018-09-25 18:30:17 +0200

    FIX scenario test

--- tests/test_account_invoice_prevent_duplicates.py
+++ tests/test_account_invoice_prevent_duplicates.py
@@ -1,36 +1,23 @@
 # This file is part of the account_invoice_prevent_duplicates module for Tryton
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
-import sys
-import re
 import unittest
 import doctest
 import trytond.tests.test_tryton
 from trytond.tests.test_tryton import ModuleTestCase
-from trytond.tests.test_tryton import doctest_teardown
-from trytond.tests.test_tryton import Py23DocChecker
-
-
-class ExceptionChecker(Py23DocChecker):
-    def check_output(self, want, got, optionflags):
-        if sys.version_info[0] > 2:
-            got = re.sub("trytond.exceptions.(.*?)", "\\1", got)
-        return Py23DocChecker.check_output(self, want, got, optionflags)
-doctest_checker = ExceptionChecker()
+from trytond.tests.test_tryton import doctest_teardown, doctest_checker
 
 
 class AccountInvoicePreventDuplicatesTestCase(ModuleTestCase):
     'Test Account Invoice Prevent Duplicates module'
     module = 'account_invoice_prevent_duplicates'
 
-
 def suite():
     suite = trytond.tests.test_tryton.suite()
     suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
         AccountInvoicePreventDuplicatesTestCase))
-    suite.addTests(doctest.DocFileSuite(
-            'scenario_invoice_prevent_duplicates.rst',
+    suite.addTests(doctest.DocFileSuite('scenario_invoice_prevent_duplicates.rst',
             tearDown=doctest_teardown, encoding='utf-8',
-            checker=doctest_checker,
-            optionflags=doctest.REPORT_ONLY_FIRST_FAILURE))
+            optionflags=doctest.REPORT_ONLY_FIRST_FAILURE,
+            checker=doctest_checker))
     return suite
